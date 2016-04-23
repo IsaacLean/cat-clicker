24d918e756279f209266abd06476612088501417
Matthew Isaac Lean <matt@mattlean.com>
2016-04-23 03:11:00 -0700

    Fulfill Cat Clicker Premium requirements

--- main.js
+++ main.js
@@ -7,49 +7,71 @@ var cat_data = {
     floyd: {
         img_loc: '/img/floyd.jpg',
         click_num: 0
+    },
+    bud: {
+        img_loc: '/img/bud.jpg',
+        click_num: 0
+    },
+    nermal: {
+        img_loc: '/img/nermal.jpg',
+        click_num: 0
+    },
+    simba: {
+        img_loc: '/img/simba.jpg',
+        click_num: 0
     }
 };
 
 /* RENDER */
-function init_cats() {
+function render_cat(cat_name_str, cat_box) {
+    cat_box.innerHTML = '';
+
+    var cat_name_str_capital = capitalize_str(cat_name_str);
+
+    var cat = document.createElement('div');
+    cat.setAttribute('id', cat_name_str);
+
+    var cat_name = document.createElement('h2');
+    cat_name.innerHTML = cat_name_str_capital;
+
+    var cat_img = document.createElement('img');
+    cat_img.setAttribute('src', cat_data[cat_name_str].img_loc);
+    cat_img.setAttribute('alt', cat_name_str_capital);
+
+    var click_counter = document.createElement('p');
+    click_counter.innerHTML = 'Clicks: ' + cat_data[cat_name_str].click_num;
+
+    cat.appendChild(cat_name);
+    cat.appendChild(cat_img);
+    cat.appendChild(click_counter);
+    cat_box.appendChild(cat);
+
+    cat.addEventListener('click', (function(cat_name_str, click_counter) {
+        return function() {
+            ++cat_data[cat_name_str].click_num;
+            click_counter.innerHTML = 'Clicks: ' + cat_data[cat_name_str].click_num;
+        }
+    })(cat_name_str, click_counter));
+}
+
+function init_cat_clicker() {
+    var cat_menu = document.getElementById('cat_menu');
     var cat_box = document.getElementById('cat_box');
 
     for(var key in cat_data) {
-        var cat = document.createElement('div');
-        cat.setAttribute('id', key);
-
-        var cat_name = document.createElement('h2');
         var cat_name_str_capital = capitalize_str(key);
-        cat_name.innerHTML = cat_name_str_capital;
 
-        var cat_img = document.createElement('img');
-        cat_img.setAttribute('src', cat_data[key].img_loc);
-        cat_img.setAttribute('alt', cat_name_str_capital);
+        // create item for cat_menu
+        var item = document.createElement('li');
+        item.innerHTML = cat_name_str_capital;
 
-        var click_counter = document.createElement('p');
-        click_counter.innerHTML = cat_data[key].click_num;
+        cat_menu.appendChild(item);
 
-        cat.appendChild(cat_name);
-        cat.appendChild(cat_img);
-        cat.appendChild(click_counter);
-        cat_box.appendChild(cat);
-
-        // OPTION #1: use event data to search DOM for specific cat's counter, then update it accordingly
-        // cat.addEventListener('click', function(evt) {
-        //     var cat_name_str_lowercase = evt.target.alt.toLowerCase();
-        //     var click_counter = document.querySelector('#' + cat_name_str_lowercase + ' p');
-
-        //     ++cat_data[cat_name_str_lowercase].click_num;
-        //     click_counter.innerHTML = cat_data[cat_name_str_lowercase].click_num;
-        // });
-
-        // OPTION #2: use IIFE (immediately-invoked function expression) to update specific cat's counter
-        cat.addEventListener('click', (function(click_num, click_counter) {
+        item.addEventListener('click', (function(cat_name_str) {
             return function() {
-                ++click_num;
-                click_counter.innerHTML = click_num;
+                render_cat(cat_name_str, cat_box);
             }
-        })(cat_data[key].click_num, click_counter));
+        })(key));
     }
 }
 
@@ -58,4 +80,4 @@ function capitalize_str(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-init_cats();
+init_cat_clicker();
